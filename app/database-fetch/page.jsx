cefe38b092b5fd909b48eb85f8eb8367b2ebad5c
DRGSLX\drago <drgs_lx@yahoo.com>
2024-09-19 10:04:32 +0300

    created the json user page mapping

--- app/database-fetch/page.jsx
+++ app/database-fetch/page.jsx
@@ -9,9 +9,9 @@ export default function Home() {
   async function fetchData() {
     setLoading(true); // Set loading to true before fetching data
     try {
-      const response = await fetch("/api/db-convert"); // Fetching from the Next.js API route
-      const result = await response.json();
-      setData(result);
+      const response = await fetch("/api/get-json"); // Fetching from the Next.js API route
+      await response.json();
+      setData(users);
     } catch (error) {
       console.error("Error fetching data:", error);
     } 
@@ -23,44 +23,19 @@ export default function Home() {
   }, []);
 
   // Extract headers from the data if it exists
-  const headers = data.length > 0 ? Object.keys(data[0]) : [];
 
   return (
     <div className="mx-auto">
-      <h1 className="text-2xl font-bold">CSV Data:</h1>
+      <h1 className="text-2xl font-bold">JSON Data:</h1>
+
       {loading ? (
         <p>Loading...</p> // Display loading message while fetching data
       ) : data.length > 0 ? (
-        <table className="min-w-full divide-y divide-gray-200">
-          <thead className="bg-gray-200">
-            <tr>
-              {headers.map((header) => (
-                <th
-                  key={header}
-                  className="px-6 py-12 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  <p className="px-2 mx-auto flex flex-row justify-between">
-                    {header}
-                  </p>
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody className="bg-white divide-y divide-gray-200 max-w-2xl">
-            {data.slice(0, 7).map((index) => (
-              <tr key={index}>
-                {headers.map((header) => (
-                  <td
-                    key={header}
-                    className="px-4 py-4 whitespace-nowrap text-sm font-medium text-gray-900"
-                  >
-                    <div className="px-12">{[header]}</div>
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div>
+          {data.map((user, index) => (
+            <p key={index}>{user.name}</p>
+          ))}
+        </div>
       ) : (
         <p>No data available yet</p>
       )}
